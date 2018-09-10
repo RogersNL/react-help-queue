839c1cb18adfd22d0bcf901dab22d0f37cafae00
Nick Rogers and Swati Sahay <swati@>
2018-09-10 13:09:00 -0700

    Add controller component.

---
+++ src/components/NewTicketControl.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ConfirmationQuestions from './ConfirmationQuestions';
+class NewTicketControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false
+    };
+    // this.handleClick = this.handleClick.bind(this);
+  }
+
+  // handleClick(){
+  //   this.setState({formVisibleOnPage: true});
+  //   console.log('formVisibleOnPage is currently set to:' + this.state.formVisibleOnPage);
+  // }
+
+  render(){
+    return (
+      <div>
+        <ConfirmationQuestions />
+      </div>
+    );
+  }
+}
+
+export default NewTicketControl;
