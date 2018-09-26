3a8e4b382d87414d17f431ca7a60dd17644bde17
Nick Rogers and Swati Sahay <nick@>
2018-09-26 11:35:09 -0700

    Add local memory as default state.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,6 +8,7 @@ import Moment from 'moment';
 import Admin from './Admin';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import c from './../constants';
 
 class App extends React.Component {
 
@@ -31,7 +32,7 @@ class App extends React.Component {
       const ticket = this.props.masterTicketList[ticketId];
       const newFormattedWaitTime = ticket.timeOpen.fromNow(true);
       const action = {
-        type: 'UPDATE_TIME',
+        type: c.UPDATE_TIME,
         id: ticketId,
         formattedWaitTime: newFormattedWaitTime
       };
